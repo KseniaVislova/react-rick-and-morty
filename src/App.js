166022aaa89e4d74765b965778c01da6021353fb
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-01-12 15:50:42 +0300

    feat: add browsing of characters provided by api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import React, { useState, useReducer, useEffect } from "react";
+
+const initialState = {
+  items: [],
+}
 
 function App() {
+  const [characters, setCharacters] = useState(initialState.items);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(async function getCharacters() {
+    try {
+      let res = await fetch("https://rickandmortyapi.com/api/character")
+      res = await res.json()
+      console.log(res)
+      setCharacters(res.results)
+      setLoading(false)
+    } catch { 
+      console.log('Error')
+    } 
+  }, [])
+
+  
+
   return (
     <div>
       <h1>Rick and Morty</h1>
+      {isLoading ? 'Загрузка данных...' :
+      <ul>
+        {
+      characters.map((item) => (
+      <li key={item.id}>
+        <h3>{item.name}</h3>
+        <p>{item.status}</p>
+        <p>{item.species}</p>
+        <p>{item.type}</p>
+      </li>  
+      ))
+    }
+      </ul>
+      }
     </div>
   );
 }

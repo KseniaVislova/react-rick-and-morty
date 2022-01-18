5b253e7eb49db3555301b19364a3048da8367672
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-01-18 21:44:01 +0300

    refactor: replace buttons to component Button

--- src/components/ButtonsList/ButtonsList.js
+++ src/components/ButtonsList/ButtonsList.js
@@ -1,13 +1,14 @@
 import styles from './ButtonsList.module.css'
+import Button from '../Button/Button';
 
 const ButtonsList = ({goToPage, buttons, pages}) => {
   return (
     <ul className={styles.list}>
-      <li><button onClick={() => goToPage(1)}>Start</button></li>
+      <li><Button onClick={() => goToPage(1)} value="Start" disabled={false}/></li>
       {buttons.map((item) => (
-        <li key={item}><button onClick={() => goToPage(item)}>{item}</button></li>
+        <li key={item}><Button onClick={() => goToPage(item)} value={item} disabled={false}/></li>
       ))}
-      <li><button onClick={() => goToPage(pages)}>End</button></li>
+      <li><Button onClick={() => goToPage(pages)} value="End" disabled={false}/></li>
     </ul>
   )
 }
